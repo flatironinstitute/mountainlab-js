13da12a5dcbf5ccfb330e766a1ab565505f76600
Jeremy Magland <jeremy.magland@gmail.com>
2018-04-26 09:01:50 -0400

    ms3.compute_templates -> ephys.compute_templates

--- examples/spike_sorting/001_simple_bash_example/view_results.py
+++ examples/spike_sorting/001_simple_bash_example/view_results.py
@@ -7,7 +7,7 @@ from mltools import mlproc as mlp
 import imp
 
 mlp.runProcess(
-    'ms3.compute_templates', # processor name
+    'ephys.compute_templates', # processor name
     {"timeseries":'data/pre.mda.prv',"firings":'data/firings.mda.prv'}, # inputs
     {"templates_out":'data/templates.mda'}, # outputs
     {"clip_size":100} # parameters
