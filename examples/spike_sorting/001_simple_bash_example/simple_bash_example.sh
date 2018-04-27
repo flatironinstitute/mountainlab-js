bae44da462aea36f4004a1727f6061757be04e8c
Jeremy Magland <jeremy.magland@gmail.com>
2018-04-27 11:28:50 -0400

    update example

--- examples/spike_sorting/001_simple_bash_example/simple_bash_example.sh
+++ examples/spike_sorting/001_simple_bash_example/simple_bash_example.sh
@@ -5,9 +5,9 @@ set -e
 mkdir -p data
 
 # Make synthetic ephys data
-ml-run-process ephys.synthesize_random_waveforms --outputs waveforms_out:data/waveforms_true.npy.prv geometry_out:data/geom.csv --parameters upsamplefac:13 M:4
+ml-run-process ephys.synthesize_random_waveforms --outputs waveforms_out:data/waveforms_true.npy.prv geometry_out:data/geom.csv --parameters upsamplefac:13 M:4 average_peak_amplitude:100
 ml-run-process ephys.synthesize_random_firings --outputs firings_out:data/firings_true.npy.prv --parameters duration:600
-ml-run-process ephys.synthesize_timeseries --inputs firings:data/firings_true.npy.prv waveforms:data/waveforms_true.npy.prv --outputs timeseries_out:data/raw.mda.prv --parameters duration:600 waveform_upsamplefac:13
+ml-run-process ephys.synthesize_timeseries --inputs firings:data/firings_true.npy.prv waveforms:data/waveforms_true.npy.prv --outputs timeseries_out:data/raw.mda.prv --parameters duration:600 waveform_upsamplefac:13 noise_level:10
 
 # Preprocess
 ml-run-process ephys.bandpass_filter --inputs timeseries:data/raw.mda.prv --outputs timeseries_out:data/filt.mda.prv --parameters samplerate:30000 freq_min:300 freq_max:6000
