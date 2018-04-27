38a077600cc4780ed949d2539c82afb58f5ea8dd
Jeremy Magland <jeremy.magland@gmail.com>
2018-04-27 06:06:20 -0400

    update

--- examples/spike_sorting/001_simple_bash_example/view_results.py
+++ examples/spike_sorting/001_simple_bash_example/view_results.py
@@ -10,17 +10,17 @@ import imp
 
 mlp.runProcess(
     'ephys.compute_templates', # processor name
-    {"timeseries":'data/pre.mda.prv',"firings":'data/firings.mda.prv'}, # inputs
-    {"templates_out":'data/templates.mda'}, # outputs
+    {"timeseries":'data/pre.mda.prv',"firings":'data/firings.npy.prv'}, # inputs
+    {"templates_out":'data/templates.npy'}, # outputs
     {"clip_size":100} # parameters
 )
 
-templates=mls.loadMdaFile('data/templates.mda')
+templates=mls.loadMdaFile('data/templates.npy')
 viz.view_templates(templates)
 
 mlp.runProcess(
     'ephys.compute_cross_correlograms',
-    {"firings":'data/firings.mda.prv'},
+    {"firings":'data/firings.npy.prv'},
     {"correlograms_out":'data/autocorrelograms.hdf5'},
     {"samplerate":30000,"max_dt_msec":50,"bin_size_msec":2,"mode":'autocorrelograms'}
 )
