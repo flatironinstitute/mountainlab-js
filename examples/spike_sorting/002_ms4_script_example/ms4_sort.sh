0b4f9fcc615e25398654e7bc8fc8146ad1992391
Jeremy Magland <jeremy.magland@gmail.com>
2018-05-15 11:27:27 -0400

    view filtered timeseries

--- examples/spike_sorting/002_ms4_script_example/ms4_sort.sh
+++ examples/spike_sorting/002_ms4_script_example/ms4_sort.sh
@@ -7,8 +7,12 @@ mkdir -p output
 PACKAGES=${ML_PACKAGE_SEARCH_DIRECTORY:-~/.mountainlab/packages}
 
 mls-run $PACKAGES/ml_ms4alg/mlscripts/ms4_v1.ml \
-	--inputs timeseries:dataset/raw.mda.prv geom:dataset/geom.csv \
-	--outputs firings_out:output/firings.mda.prv \
+	--inputs \
+		timeseries:dataset/raw.mda.prv \
+		geom:dataset/geom.csv \
+	--outputs \
+		firings_out:output/firings.mda.prv \
+		filt_out:output/filt.mda.prv \
 	--parameters \
 		samplerate:30000 \
 		detect_sign:1 \
@@ -17,7 +21,7 @@ mls-run $PACKAGES/ml_ms4alg/mlscripts/ms4_v1.ml \
 		filter:true whiten:true
 
 ml-run-process ephys.compute_templates \
-	--inputs timeseries:dataset/raw.mda.prv firings:output/firings.mda.prv \
+	--inputs timeseries:output/filt.mda.prv firings:output/firings.mda.prv \
 	--outputs templates_out:output/templates.mda.prv \
 	--parameters \
 		clip_size:150
