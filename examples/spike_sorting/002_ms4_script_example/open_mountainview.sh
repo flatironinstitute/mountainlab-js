568fdcffaa5f42c2ddf5fc15523c5871d5a2fbb0
Jeremy Magland <jeremy.magland@gmail.com>
2018-06-27 18:36:30 -0400

    cleaning up

--- examples/spike_sorting/002_ms4_script_example/open_mountainview.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qt-mountainview --filt output/filt.mda.prv --firings output/firings.mda.prv --samplerate 30000
--- examples/spike_sorting/002_ms4_script_example/view_filtered_timeseries.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ev-view-timeseries output/filt.mda.prv --firings output/firings.mda.prv
