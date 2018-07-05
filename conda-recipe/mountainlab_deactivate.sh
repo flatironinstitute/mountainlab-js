34b125b62bc1aa87793bea5487df7fee03b1239e
Tom Davidson <tjd@alum.mit.edu>
2018-07-05 10:36:32 -0700

    conda build: manage package install dirs; remove de/activate messages

--- conda-recipe/mountainlab_deactivate.sh
+++ conda-recipe/mountainlab_deactivate.sh
@@ -1,19 +1,22 @@
-#!/bin/bash -e
-echo "=== [ mountainlab-js deactivation ] ==========================================="
-echo ":"
-mldir_conda="${CONDA_PREFIX?}"/etc/mountainlab
+#!/bin/bash
+#echo "=== [ mountainlab-js deactivation ] ==========================================="
+#echo ":"
 
 # if we set confdir on activation, and it hasn't changed, unset it.
-if [ -n "$MLCONFDIR_WASEMPTY" ] && [ "$ML_CONFIG_DIRECTORY" = "$mldir_conda" ]; then
-    echo ": Deactivating mountainlab search path for env ${CONDA_PROMPT_MODIFIER}."
+if [ -n "$ML_CONFDIR_WASEMPTY" ] && [ "$ML_CONFIG_DIRECTORY" = "$ML_CONDA_DIR" ]; then
+#    echo ": Deactivating mountainlab search path for env (${CONDA_DEFAULT_ENV})."
     unset ML_CONFIG_DIRECTORY 
-    unset MLCONFDIR_WASEMPTY
 else 
     if [ -n "$ML_CONFIG_DIRECTORY" ]; then
-	echo ": User-set ML_CONFIG_DIRECTORY left unchanged ( '$ML_CONFIG_DIRECTORY' )."
+	echo ": Original user-set ML_CONFIG_DIRECTORY left unchanged ( '$ML_CONFIG_DIRECTORY' )."
     else
 	echo ": ML_CONFIG_DIRECTORY already unset/null, nothing to do"
     fi
 fi
-echo ":"
-echo "==============================================================================="
+#echo ":"
+#echo "==============================================================================="
+
+unset ML_CONFDIR_WASEMPTY
+unset ML_CONDA_DIR
+unset ML_CONDA_PACKAGES_DIR
+unset ML_CONDA_DATABASE_DIR
