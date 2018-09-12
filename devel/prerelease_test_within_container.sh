8ea8e734882bfc9f9cd5e111521caddd31aab6e2
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-12 10:23:12 -0400

    fix test

--- devel/prerelease_test_within_container.sh
+++ devel/prerelease_test_within_container.sh
@@ -9,6 +9,6 @@ pip install ml_ephys
 ml-link-python-module ml_ephys `ml-config package_directory`/ml_ephys
 ml-list-processors
 
-ml-run-process ephys.synthesize_random_waveforms --o waveforms_out:waveforms.mda
+ml-run-process ephys.synthesize_random_waveforms -o waveforms_out:waveforms.mda
 mda-info waveforms.mda
 
