9e7f681bab4578a110f8f043e75f42c4cd171fe2
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-12 10:18:24 -0400

    changing deployment system

---
+++ devel/prerelease_test_within_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH=$PWD/bin:$PATH
+
+ml-config
+ml-list-processors
+
+pip install ml_ephys
+ml-link-python-module ml_ephys `ml-config package_directory`/ml_ephys
+ml-list-processors
+
+ml-run-process ephys.synthesize_random_waveforms --waveforms_out=waveforms.mda
+mda-info waveforms.mda
+
