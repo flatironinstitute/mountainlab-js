f4c14aa2c9aea3e4e530cf35750b4a482233d004
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-12 23:23:54 -0400

    deploy to anaconda

--- devel/travis_deploy.sh
+++ devel/travis_deploy.sh
@@ -5,3 +5,7 @@ set -e
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 docker pull magland/mldevel_publish
 docker run -v $PWD/.npmrc:/root/.npmrc -v $PWD:/source magland/mldevel_publish npm
+
+echo "$ANACONDA_API_TOKEN" > .anaconda
+docker pull magland/mldevel_publish
+docker run -v $PWD/.anaconda:/root/.anaconda -v $PWD:/source magland/mldevel_publish conda
