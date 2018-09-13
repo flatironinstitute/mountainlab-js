f71293a5eedd46cae0b4a6b544e9054502a418c0
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-13 11:35:21 -0400

    update travis deploy

--- devel/travis_deploy.sh
+++ devel/travis_deploy.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+# publish to npm
+# you must add encrypted NPM_TOKEN env variable to .travis.yml
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
 docker pull magland/mldevel_publish
 docker run -v $PWD/.npmrc:/root/.npmrc -v $PWD:/source magland/mldevel_publish npm
 
-echo "$ANACONDA_API_TOKEN" > .anaconda
+# publish to anaconda
+# you must add encrypted ANACONDA_API_TOKEN env variable to .travis.yml
 docker pull magland/mldevel_publish
-docker run -v $PWD/.anaconda:/root/.anaconda -v $PWD:/source magland/mldevel_publish conda
+docker run -e "ANACONDA_API_TOKEN=$ANACONDA_API_TOKEN" -v $PWD:/source magland/mldevel_publish conda
